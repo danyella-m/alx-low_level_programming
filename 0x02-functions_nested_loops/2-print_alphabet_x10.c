a1036a55309dda168f826d7ebf50d5987f4bb725
danyella-m <mabokeladanyella@gmail.com>
2023-02-22 05:20:57 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
 	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
 	 * followed by a new line
 	 */
-	int void print_alphabet_x10(void)
+	int main(void) print_alphabet_x10(void)
 	{
 		char ch;
 		int i;
